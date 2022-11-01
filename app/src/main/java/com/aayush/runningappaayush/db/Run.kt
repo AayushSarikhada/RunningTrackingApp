4ff0e00f61ac2a891e611077bc8f63a64ca09fe3
aayush <ayush.sarikhada@gmail.com>
2022-11-01 17:57:44 +0530

    video no 3

--- app/src/main/java/com/aayush/runningappaayush/db/Run.kt
+++ app/src/main/java/com/aayush/runningappaayush/db/Run.kt
@@ -1,4 +1,19 @@
 package com.aayush.runningappaayush.db
 
-class Run {
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img:Bitmap? = null,
+    var timeStamp:Long = 0L,            //when our run was done
+    var avgSpeedInKMH:Float = 0f,
+    var distanceInMeters:Int = 0,
+    var timeInMillis:Long = 0L,        //how long our run was
+    var caloriesBurned:Int = 0
+){
+    @PrimaryKey(autoGenerate = true)
+    var id:Int? = null
 }
